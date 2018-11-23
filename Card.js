6ad51cea7c7c86fa72a7face2b177e21745fee17
tomcorey26 <tomcorey26@gmail.com>
2018-11-23 15:05:41 -0500

    constructor for card

--- Card.js
+++ Card.js
@@ -1,5 +1,24 @@
 module.exports = class Card {
-  constructuctor() {
+  constructor(suite,rank) {
+    this.suite = suite;
+    this.rank = rank;
 
+    this.lookup = {
+      "10":"T",
+      "11":"J",
+      "12":"Q",
+      "13":"K",
+      "14":"A",
+    }
+  }
+  print() {
+    if(this.lookup[this.rank]){
+      return this.lookup[this.rank] + " " +this.suite;
+    }
+    else {
+      return this.rank + " " + this.suite;
+    }
+    
   }
 }
+
