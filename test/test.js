d486c8871d6fe4d264792c94238367b9e5ff58a2
tomcorey26 <tomcorey26@gmail.com>
2018-11-23 17:02:30 -0500

    Add test for dealCard

--- test/test.js
+++ test/test.js
@@ -4,6 +4,11 @@ const Deck = require('../Deck');
 
 describe('Deck', () => {
   let deck = new Deck();
+  beforeEach(() => {
+    deck.makePristine();
+  });
+  
+
   describe('init', () => {
     it('should contain 52 cards', ()=> {
       assert.equal(deck.cards.length, 52);
@@ -13,4 +18,14 @@ describe('Deck', () => {
       assert.equal(deck.cards[0].constructor.name, 'Card');
     });
   });
+
+  describe('dealCard', () => {
+    it('should remove card from deck', () => {
+      let card = deck.dealCard();
+      assert.equal(deck.cards.length, 51);
+    })
+    it('should have a card in its array', ()=> {
+      assert.equal(deck.cards[0].constructor.name, 'Card');
+    });
+  })
 });
