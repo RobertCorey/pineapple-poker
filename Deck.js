9a00710c44bc220398e03c6b155368375dc70baa
tomcorey26 <tomcorey26@gmail.com>
2018-11-23 16:02:45 -0500

    changes to deck

---
+++ Deck.js
@@ -0,0 +1,21 @@
+let Card = require('./Card');
+
+module.exports = class Deck {
+    constructor(){
+        this.cards = []
+
+        let suites = ["d","c","h","s"];
+        for(let i = 0; i < 4 ; i++){
+            for(let j=2 ; j<15 ; j++){
+
+                let card1 = new Card(suites[i],j);
+                this.cards.push(card1);
+                
+            }
+        }
+
+        
+    }
+
+
+}
